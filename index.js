ffb96d7a32af8113f97ac903778b8098090aa904
Shahinadev <dev.shahin2@gmail.com>
2021-11-01 01:28:05 +0600

    first coomit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 //app listening
 app.get("/", (req, res) => {
-  res.send("Server is running.");
+  res.send("Server is running.hey");
 });
 
 app.listen(PORT, () => {
