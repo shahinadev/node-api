e3ed831a680ac89adf3e5b48cd291c98d7aad77c
Shahinadev <dev.shahin2@gmail.com>
2021-11-01 01:24:21 +0600

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require("cors");
-
 app.use(cors());
+const PORT = process.env.PORT || 8080;
 
 //app listening
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("server is running");
 });
